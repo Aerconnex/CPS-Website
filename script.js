4e8a72b466a13eff29c014c6168583903edfce4e
Aerconnex <159484455+Aerconnex@users.noreply.github.com>
2024-05-17 07:41:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const video = document.getElementById("video");
 const videoContainer = document.getElementById("video-container");
-const parallax = document.getElementById("parallax");
 const main = document.querySelector('main')
 
 video.onended = function() {
   videoContainer.classList.add("hidden"); 
-  parallax.classList.remove("hidden"); 
   document.body.style.overflow = 'auto'; 
   main.style.overflow = 'auto'; 
 };
